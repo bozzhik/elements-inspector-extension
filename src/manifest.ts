3bd5a7d80a03bb9a30848e1a8dc37989599babc7
maxim <bozzhik@ya.ru>
2024-09-27 22:55:37 +0300

    init `extension` project using `create-chrome-ext`
    https://github.com/guocaoyi/create-chrome-ext

---
+++ src/manifest.ts
@@ -0,0 +1,39 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import packageData from '../package.json'
+
+//@ts-ignore
+const isDev = process.env.NODE_ENV == 'development'
+
+export default defineManifest({
+  name: `${packageData.displayName || packageData.name}${isDev ? ` ➡️ Dev` : ''}`,
+  description: packageData.description,
+  version: packageData.version,
+  manifest_version: 3,
+  icons: {
+    16: 'img/logo-16.png',
+    32: 'img/logo-34.png',
+    48: 'img/logo-48.png',
+    128: 'img/logo-128.png',
+  },
+  action: {
+    default_popup: 'src/views/popup.html',
+    default_icon: 'img/logo-48.png',
+  },
+  background: {
+    service_worker: 'src/services/background.ts',
+    type: 'module',
+  },
+  content_scripts: [
+    {
+      matches: ['http://*/*', 'https://*/*'],
+      js: ['src/services/content-script.ts'],
+    },
+  ],
+  web_accessible_resources: [
+    {
+      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      matches: [],
+    },
+  ],
+  permissions: ['storage'],
+})
