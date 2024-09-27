e2c77670cb52ca8125afdb904205e103e4eddf5c
maxim <bozzhik@ya.ru>
2024-09-28 02:34:01 +0300

    add `font` inspection feature into `Popup` module

--- src/services/content-script.ts
+++ src/services/content-script.ts
@@ -1,1 +1,20 @@
-console.info('contentScript is running')
+console.info('Content script is running')
+
+import { TFontsData } from '@/lib/types'
+
+const inspectFonts = (): TFontsData => {
+  const fontInfo = new Set<string>()
+  const elements = document.querySelectorAll<HTMLElement>('*')
+
+  elements.forEach((el) => {
+    const computedStyle = window.getComputedStyle(el)
+    computedStyle.fontFamily.split(',').forEach((font) => {
+      fontInfo.add(font.trim())
+    })
+  })
+  return Array.from(fontInfo)
+}
+
+const fonts = inspectFonts()
+chrome.storage.sync.set({ fonts })
+chrome.runtime.sendMessage({ type: 'FONT_INFO', fonts })
