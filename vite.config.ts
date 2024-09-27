42cee8bb8448409173850b078d0dcd7f1c3e51d0
maxim <bozzhik@ya.ru>
2024-09-27 23:02:55 +0300

    fix establish `web-socket` connection
    https://github.com/guocaoyi/create-chrome-ext/issues/74

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,13 @@ import manifest from './src/manifest'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    server: {
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        port: 5173,
+      },
+    },
     build: {
       emptyOutDir: true,
       outDir: 'build',
